7945a2736783d0b7b1663685e407e0791a7d20b9
Amir Khalilzadeh <amirh.khalilzadeh@gmail.com>
2022-09-14 17:40:35 +0200

    refined the code and text
    refined the code and text

--- timeseries_regression_case_study/tools.py
+++ timeseries_regression_case_study/tools.py
@@ -1,29 +1,24 @@
 # Import libraries
-
-import pandas as pd
-import numpy as np
-pd.set_option('max_colwidth', 2000)
-
 import warnings
 warnings.filterwarnings("ignore")
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pandas.plotting import lag_plot
-import statsmodels.api as sm
-sns.set_context('talk')
-
-import os
-from sklearn.linear_model import Ridge
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error as MAE
-
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import Pipeline
+from pandas.plotting import lag_plot
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
+sns.set_context('notebook')
 
 ####################################
 ### Function: load_data         ####
@@ -54,29 +49,24 @@ def info_data(df):
     print('---------------------------------------')
     print('Number of missing hours: ' ,list(df.isna().sum()))
     
-    
-    
-    
 ####################################
 ### Function: plot_data         ####
 ####################################
 
-def plot_data():
+def plot_data(data):
     
-    # load data and rename columns
-    df = pd.read_csv(os.path.join('data','energy_data.csv'), index_col=0, parse_dates=True)
-    df.rename(columns={"load": "electric load", "temp": "temperature"},inplace=True)
+    df = data.copy()
     ###################################
     # plotting distributions
     fig, ax = plt.subplots(1, 2, figsize=(15, 4), gridspec_kw={"wspace": 0.2})
     
     ax[0].hist(df["electric load"],bins=30,color='skyblue')
-    ax[0].set_title("distribution of electric load",fontsize=14)
-    ax[0].tick_params(labelsize=12)
+    ax[0].set_title("distribution of electric load")
+    ax[0].tick_params()
     
     ax[1].hist(df["temperature"],bins=30,color='skyblue')
-    ax[1].set_title("distribution of temperature",fontsize=14)
-    ax[1].tick_params(labelsize=12)
+    ax[1].set_title("distribution of temperature")
+    ax[1].tick_params()
 
     plt.show()
     ####################################    
@@ -84,21 +74,21 @@ def plot_data():
     fig, ax = plt.subplots(2, 1, figsize=(15, 7), gridspec_kw={"hspace": 0.6})
 
     ax[0].plot(df["electric load"],color='skyblue')
-    ax[0].set_title("hourly electric load",fontsize=14)
-    ax[0].set_ylabel("electric load (MW)",fontsize=14)
-    ax[0].tick_params(labelsize=12)
+    ax[0].set_title("hourly electric load")
+    ax[0].set_ylabel("electric load (MW)")
+    ax[0].tick_params()
     
     ax[1].plot(df["temperature"],color='skyblue')
-    ax[1].set_title("hourly temperature",fontsize=14)
-    ax[1].set_ylabel("temperature",fontsize=14)
-    ax[1].tick_params(labelsize=12)
+    ax[1].set_title("hourly temperature")
+    ax[1].set_ylabel("temperature")
+    ax[1].tick_params()
 
     plt.show()
     ####################################    
     # create time features
     df["month"] = df.index.month - 1
-    df["day"] = df.index.day_of_week
-    df["hour"] = df.index.hour
+    df["day"]   = df.index.day_of_week
+    df["hour"]  = df.index.hour
     
     # plot aggregate trends    
     fig, ax = plt.subplots(
@@ -109,19 +99,19 @@ def plot_data():
     ax[1].plot(df.groupby("day").mean()["electric load"],color='skyblue')
     ax[2].plot(df.groupby("hour").mean()["electric load"],color='skyblue')
 
-    ax[0].set_title("average loads over the months",fontsize=14)
-    ax[1].set_title("average loads over the week",fontsize=14)
-    ax[2].set_title("average loads during the day",fontsize=14)
+    ax[0].set_title("average loads over the months")
+    ax[1].set_title("average loads over the week")
+    ax[2].set_title("average loads during the day")
 
-    ax[0].set_xlabel("January to December",fontsize=14)
-    ax[1].set_xlabel("Monday to Sunday",fontsize=14)
-    ax[2].set_xlabel("0 to 23 hours",fontsize=14)
+    ax[0].set_xlabel("January to December")
+    ax[1].set_xlabel("Monday to Sunday")
+    ax[2].set_xlabel("0 to 23 hours")
     
-    ax[0].tick_params(labelsize=12)
-    ax[1].tick_params(labelsize=12)
-    ax[2].tick_params(labelsize=12)
+    ax[0].tick_params()
+    ax[1].tick_params()
+    ax[2].tick_params()
 
-    ax[0].set_ylabel("electric loads (MW)",fontsize=14)
+    ax[0].set_ylabel("electric loads (MW)")
     plt.show()
     
     ####################################    
@@ -136,9 +126,9 @@ def plot_data():
     heatmap.index = df.index.month_name().unique().tolist()
     
     sns.heatmap(heatmap, cmap="YlGnBu")
-    axes.set_title("electric load per month and hour",fontsize=14)
-    axes.tick_params(labelsize=11)
-    axes.set_xlabel("hours",fontsize=14)
+    axes.set_title("electric load per month and hour")
+    axes.tick_params()
+    axes.set_xlabel("hours")
     plt.show()
     
     
@@ -146,15 +136,12 @@ def plot_data():
 ### Function: plot_corr         ####
 ####################################
 
-def plot_corr(n_lags):    
+def plot_corr(data, n_lags):    
 
-    # load data and rename columns
-    df = pd.read_csv(os.path.join('data','energy_data.csv'), index_col=0, parse_dates=True)
-    df.rename(columns={"load": "electric load", "temp": "temperature"},inplace=True)
-    
+    df = data.copy()
     ###################################
 
-    # create 1 hour lags
+    # create lags
     df["load_lag"] = df["electric load"].shift(n_lags)
     df["temp_lag"] = df["temperature"].shift(n_lags)
 
@@ -170,9 +157,9 @@ def plot_corr(n_lags):
    #  plot autocorrelations
     sm.graphics.tsa.plot_acf(df["electric load"], lags=24, alpha=0.05, zero=False, ax=axes)
 
-    axes.set_xlabel("1 to 24 hours lags",fontsize=14)
-    axes.set_ylabel("autocorrelations",fontsize=14)
-    axes.set_title("electric loads autocorrelations, shaded area shows 95% confidence bands",fontsize=14)
+    axes.set_xlabel("1 to 24 hours lags")
+    axes.set_ylabel("autocorrelations")
+    axes.set_title("electric loads autocorrelations, shaded area shows 95% confidence bands")
 
     # red marker for the n_lags
     axes.scatter(n_lags, df["electric load"].squeeze().autocorr(n_lags), marker="x", c="red", s=50 * 2 ** 2)
@@ -190,15 +177,15 @@ def plot_corr(n_lags):
     lag_plot(df["electric load"], lag=n_lags, ax=axes[0], alpha=0.02)
     axes[1].scatter(df["temp_lag"], df["electric load"], alpha=0.02)
 
-    axes[0].set_xlabel(r'electric load {} hours ago'.format(n_lags),fontsize=14)
-    axes[0].set_ylabel("current load",fontsize=14)
-    axes[0].set_title(r"auto-correlation: {:.3f}".format(df["electric load"].squeeze().autocorr(n_lags)),fontsize=14)
+    axes[0].set_xlabel(r'electric load {} hours ago'.format(n_lags))
+    axes[0].set_ylabel("current load")
+    axes[0].set_title(r"auto-correlation: {:.3f}".format(df["electric load"].squeeze().autocorr(n_lags)))
 
-    axes[1].set_xlabel(r'temperature {} hours ago'.format(n_lags),fontsize=14)
-    axes[1].set_title(r"correlation: {:.3f}".format(df["electric load"].corr(df["temp_lag"])),fontsize=14)
+    axes[1].set_xlabel(r'temperature {} hours ago'.format(n_lags))
+    axes[1].set_title(r"correlation: {:.3f}".format(df["electric load"].corr(df["temp_lag"])))
 
-    axes[0].tick_params(labelsize=12)
-    axes[1].tick_params(labelsize=12)
+    axes[0].tick_params()
+    axes[1].tick_params()
  
     plt.show()
     
@@ -207,12 +194,11 @@ def plot_corr(n_lags):
 ### Function: sample_split             ####
 ###########################################
 
-def sample_split():
+def sample_split(data):
+       
+    df = data.copy()
+    ###################################
     
-    # load data and rename columns
-    df = pd.read_csv(os.path.join('data','energy_data.csv'), index_col=0, parse_dates=True)
-    df.rename(columns={"load": "electric load", "temp": "temperature"},inplace=True)
-   
     # create time features
     df["month"] = df.index.month - 1
     df["day"] = df.index.day_of_week
@@ -240,10 +226,10 @@ def sample_split():
     ax.plot(train['electric load'], label="Train set", color="skyblue")
     ax.plot(test['electric load'], label="Test set", color="lightsalmon")
 
-    ax.set_title("train and test sets",fontsize=14)
-    ax.set_ylabel("electric loads (MW)",fontsize=14)
-    ax.tick_params(labelsize=12)
-    ax.legend(fontsize=14)
+    ax.set_title("train and test sets")
+    ax.set_ylabel("electric loads (MW)")
+    ax.tick_params()
+    ax.legend()
     plt.show()
     
     return train, test
@@ -284,12 +270,12 @@ def train_model(train, select_model):
 
         # Create the grid search object
         fit_model = GridSearchCV(
-            pipe_ridge,
-            grid,
-            cv=cv,
-            return_train_score=True,
-            scoring="neg_mean_absolute_error",
-            verbose=0)
+                                pipe_ridge,
+                                grid,
+                                cv=cv,
+                                return_train_score=True,
+                                scoring="neg_mean_absolute_error",
+                                verbose=0)
 
         print('Grid search is setup!')
         
@@ -328,11 +314,11 @@ def train_model(train, select_model):
             c="red",
             zorder=10,
         )
-        ax.set_xlabel("$alpha$",fontsize=14)
-        ax.set_ylabel("MAE",fontsize=14)
-        ax.set_title("Evolution of training/validation errors with $alpha$",fontsize=14)
+        ax.set_xlabel("$alpha$")
+        ax.set_ylabel("MAE")
+        ax.set_title("Evolution of training/validation errors with $alpha$")
 
-        ax.legend(fontsize=14)
+        ax.legend()
         plt.show()
         
     else:
@@ -346,23 +332,23 @@ def train_model(train, select_model):
         pipe_rf = Pipeline(
             [
                 ("preprocessor", preprocessor),
-                ("rf", RandomForestRegressor(min_samples_split=20)),
+                ("rf", RandomForestRegressor(min_samples_split=10, max_depth=15)),
             ]
         )
         print('Preprocessing data is done!')
         print('Cross-validation is setup!')
 
         # Create grid for alpha
-        grid = {"rf__n_estimators": np.arange(1, 1001, 100)}
+        grid = {"rf__n_estimators": np.arange(1, 152, 30)}
 
         # Create the grid search object
         fit_model = GridSearchCV(
-            pipe_rf,
-            grid,
-            cv=cv,
-            return_train_score=True,
-            scoring="neg_mean_absolute_error",
-            verbose=0)
+                                pipe_rf,
+                                grid,
+                                cv=cv,
+                                return_train_score=True,
+                                scoring="neg_mean_absolute_error",
+                                verbose=0)
         
         print('Grid search is setup!')
         
@@ -370,9 +356,11 @@ def train_model(train, select_model):
         fit_model.fit(X_tr, y_tr)
         
         print('Training with grid search and cross-validation is done!')
-        print('A total of {} models are fitted'.format(n_split * len(grid['rf__n_estimators'])))  
+        print('A total of {} models were fitted'.format(n_split * len(grid['rf__n_estimators'])))  
 
+        print('\n')
         print('Here is the results of grid search:')
+        print('\n')
         
         # Collect results in a DataFrame
         cv_results = pd.DataFrame(fit_model.cv_results_)
@@ -397,10 +385,10 @@ def train_model(train, select_model):
             c="red",
             zorder=10,
         )
-        ax.set_xlabel("number of trees",fontsize=14)
-        ax.set_ylabel("MAE",fontsize=14)
-        ax.set_title("Evolution of training/validation errors with number of trees",fontsize=14)
-        ax.legend(fontsize=14)
+        ax.set_xlabel("number of trees")
+        ax.set_ylabel("MAE")
+        ax.set_title("Evolution of training/validation errors with number of trees")
+        ax.legend()
         plt.show()
 
     return fit_model
@@ -450,7 +438,6 @@ def evaluate_model(model, train, test, n_days):
     axes[0].barh(np.arange(3), [baseline, baseline_lag, model_mae],color='skyblue', fill=False)
     axes[0].set_yticks(np.arange(3))
     axes[0].set_yticklabels(("median as baseline", "previous hour as baseline", "model"))
-#    axes[0].set_xlabel("MAE")
     axes[0].set_title("mean absolute error (MAE) for test data")    
     axes[0].spines['top'].set_visible(False)
     axes[0].spines['right'].set_visible(False)
